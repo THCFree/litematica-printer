ee7d776f09cc5dbed0cef9cf85fe74241232a89e
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2023-09-08 23:57:29 +0200

    Fix block placements

--- v1_20/src/main/java/me/aleksilassila/litematica/printer/v1_20/Printer.java
+++ v1_20/src/main/java/me/aleksilassila/litematica/printer/v1_20/Printer.java
@@ -9,7 +9,6 @@ import me.aleksilassila.litematica.printer.v1_20.actions.PrepareAction;
 import me.aleksilassila.litematica.printer.v1_20.config.PrinterConfig;
 import me.aleksilassila.litematica.printer.v1_20.guides.Guide;
 import me.aleksilassila.litematica.printer.v1_20.guides.Guides;
-import me.aleksilassila.litematica.printer.v1_20.guides.placement.GeneralPlacementGuide;
 import me.aleksilassila.litematica.printer.v1_20.implementation.actions.InteractActionImpl;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -22,6 +21,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Printer {
     @NotNull
@@ -87,7 +87,41 @@ public class Printer {
         return false;
     }
 
+    private List<BlockPos> getBlocksPlayerOccupied() {
+        ArrayList<BlockPos> positions = new ArrayList<>();
+        BlockPos playerPos = player.getBlockPos();
+        int blocksHeightOccupied = (int) Math.ceil(player.getPos().y + player.getHeight() - playerPos.getY());
+
+        positions.add(player.getBlockPos());
+        positions.add(player.getBlockPos().up());
+        if (blocksHeightOccupied > 2) {
+            positions.add(playerPos.up(2));
+        }
+        if (Math.floor(player.getPos().x + player.getWidth() / 2) > playerPos.getX()) {
+            for (int i = 0; i < blocksHeightOccupied; i++) {
+                positions.add(playerPos.up(i).east());
+            }
+        }
+        if ((player.getPos().x - player.getWidth() / 2) < playerPos.getX()) {
+            for (int i = 0; i < blocksHeightOccupied; i++) {
+                positions.add(playerPos.up(i).west());
+            }
+        }
+        if (Math.floor(player.getPos().z + player.getWidth() / 2) > playerPos.getZ()) {
+            for (int i = 0; i < blocksHeightOccupied; i++) {
+                positions.add(playerPos.up(i).south());
+            }
+        }
+        if ((player.getPos().z - player.getWidth() / 2) < playerPos.getZ()) {
+            for (int i = 0; i < blocksHeightOccupied; i++) {
+                positions.add(playerPos.up(i).north());
+            }
+        }
+        return positions.stream().distinct().toList();
+    }
+
     private List<BlockPos> getReachablePositions() {
+        List<BlockPos> playerOccupied = getBlocksPlayerOccupied();
         int maxReach = (int) Math.ceil(LitematicaMixinMod.PRINTING_RANGE.getDoubleValue());
         double maxReachSquared = MathHelper.square(LitematicaMixinMod.PRINTING_RANGE.getDoubleValue());
 
@@ -109,10 +143,7 @@ public class Printer {
         }
 
         return positions.stream()
-                .filter(p -> {
-                    Vec3d vec = Vec3d.ofCenter(p);
-                    return this.player.getPos().squaredDistanceTo(vec) > 1 && this.player.getEyePos().squaredDistanceTo(vec) > 1;
-                })
+                .filter(p -> playerOccupied.stream().noneMatch(p::equals))
                 .sorted((a, b) -> {
                     double aDistance = this.player.getPos().squaredDistanceTo(Vec3d.ofCenter(a));
                     double bDistance = this.player.getPos().squaredDistanceTo(Vec3d.ofCenter(b));
